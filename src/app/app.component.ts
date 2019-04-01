2029c923f887fb70f87b2fa36f8373bebab315ab
supermagicmike <mickaelcommerot1@gmail.com>
2019-04-02 00:37:46 +0200

    Mike is back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,46 @@
-import { Component } from "@angular/core";
-import { World, Product,Pallier } from "./world";
+import { Component, OnInit } from "@angular/core";
+import { World, Product, Pallier } from "./world";
 import { RestserviceService } from "./restservice.service";
+import { ToasterModule, ToasterService } from "angular2-toaster";
+import { strictEqual } from "assert";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "captitalist-cli";
   world: World = new World();
   server: string;
   qtmulti: string;
+  username: string;
   price = ["x1", "x10", "x100", "max"];
-  constructor(private service: RestserviceService) {
+  toasterService: ToasterService;
+  New: String = "";
+
+  constructor(
+    private service: RestserviceService,
+    toasterService: ToasterService
+  ) {
     this.server = service.getServer();
     service.getWorld().then(world => {
       this.world = world;
       this.qtmulti = this.price[0];
     });
+    this.toasterService = toasterService;
+  }
+  ngOnInit() {
+    this.username = localStorage.getItem("username");
+    if (this.username == null) {
+      this.username = "captain" + Math.floor(Math.random() * 10000);
+      console.log("username jsuis al" + this.username);
+      localStorage.setItem("username", this.username);
+    }
   }
 
   onProductionDone(p: Product) {
+    console.log("username :" + this.username);
     this.world.money += p.revenu;
     this.world.score += p.revenu;
   }
@@ -39,20 +58,32 @@ export class AppComponent {
     }
   }
 
-  hire(manager:Pallier){
-    if (this.world.money>manager.seuil){
-      this.world.money-=manager.seuil;
-      manager.unlocked=true;
+  hire(manager: Pallier) {
+    if (this.world.money > manager.seuil) {
+      this.world.money -= manager.seuil;
+      manager.unlocked = true;
       this.world.products.product.forEach(element => {
-        console.log("elements : ");
-        console.log("elements id : "+element.id);
-        console.log("idcible: "+manager.idcible);
-        console.log(element.id==manager.idcible);
-        if(element.id==manager.idcible){
-          element.managerUnlocked=true;
-          console.log("element in if : "+element+ "id : "+element.id)
-        }       
+        if (element.id == manager.idcible) {
+          element.managerUnlocked = true;
+          this.toasterService.pop("success", "Manager hired ! ", element.name);
+        }
       });
     }
+  }
+  onUsernameChanged() {
+    localStorage.setItem("username", this.username);
+    this.service.setUser(this.username);
+  }
+
+  checkNewManager() {
+    let res = false;
+    this.world.managers.pallier.forEach(value => {
+      if (!value.unlocked) {
+        if (value.seuil < this.world.money) {
+          res = true;
+        }
+      }
+    });
+    return res;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ProductComponent } from "./product/product.component";
 import { RestserviceService } from "./restservice.service";
-import { BigvaluePipe } from './bigvalue.pipe';
-import { ModalComponent } from './modal/modal.component';
-
+import { BigvaluePipe } from "./bigvalue.pipe";
+import { ModalComponent } from "./modal/modal.component";
+import { FormsModule } from "@angular/forms";
+import { ToasterModule, ToasterService } from "angular2-toaster";
 @NgModule({
   declarations: [AppComponent, ProductComponent, BigvaluePipe, ModalComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [RestserviceService],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToasterModule
+  ],
+  providers: [RestserviceService, ToasterService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
